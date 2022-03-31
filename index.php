2e19df7eb658131179487ee5451c3dcd17811187
yunnor <japan.ruan@gmail.com>
2022-03-31 09:30:34 -0300

    Admin - Esqueceu a senha

--- index.php
+++ index.php
@@ -153,7 +153,73 @@ $app->post("/admin/users/:iduser", function($iduser){
 	
 });
 
+$app->get("/admin/forgot", function(){
 
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+});
+
+$app->get("/admin/forgot/sent", function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+});
+
+$app->get("/admin/forgot/reset", function(){
+	
+	$user = User::validForgotDecrypt($_GET["code"]);
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forget-sent", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+
+});
+
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT,[
+		"cost"=>12
+	]);
+
+	$user->setPassword($_POST["password"]);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forget-reset-success");
+
+});
 
 $app->run();
 
---
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Mailer{
+
+    const USERNAME = "japan.ruan@gmail.com";
+    const PASSWORD = "RSRlocinho1.";
+    const NAME_FROM = "PHP Ecommerce";
+    private $mail;
+    public function __construct($toAddress, $toName, $subject, $tplName, $data = array())
+    {
+        $config = array(
+            "tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/email/",
+            "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
+            "debug"         => false
+        );
+        Tpl::configure( $config );
+        $tpl = new Tpl;
+        foreach ($data as $key => $value) { 
+            $tpl->assign($key, $value);
+        }
+        $html = $tpl->draw($tplName, true);
+        $this->mail = new \PHPMailer;
+        $this->mail->CharSet = 'UTF-8';
+        $this->mail->isSMTP();
+        $this->mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        $this->mail->SMTPDebug = 0;
+        $this->mail->Debugoutput = 'html';
+        $this->mail->Host = 'smtp.gmail.com';
+        $this->mail->Port = 587;
+        $this->mail->SMTPSecure = 'tls';
+        $this->mail->SMTPAuth = true;
+        $this->mail->Username = Mailer::USERNAME;
+        $this->mail->Password = Mailer::PASSWORD;
+        $this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
+        $this->mail->addAddress($toAddress, $toName);
+        $this->mail->Subject = $subject;
+        $this->mail->msgHTML($html);
+        $this->mail->AltBody = 'This is a plain-text message body';
+    }
+    public function send()
+    {
+        return $this->mail->send();
+    }
+}
+
+}
+
+?>
